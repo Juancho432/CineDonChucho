81df3ecc28e5f5d487359750dafc0189da5f17cc
Emmanuel Mora <emmanuel.mora388@pascualbravo.edu.co>
2025-09-06 10:13:57 -0500

    Configuracion de cadena de conexion con SQL Server

--- CapaDatos/CD_Pelicula.cs
+++ CapaDatos/CD_Pelicula.cs
@@ -4,9 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
 namespace CapaDatos
 {
     public class CD_Pelicula
     {
+        //Indicar la cadena de conexion
+        private static readonly string cadena =
+            ConfigurationManager.ConnectionStrings["MiConexion"].ConnectionString;
     }
 }
